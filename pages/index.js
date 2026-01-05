cf98a65f70c1c1ec66b5b533489ec9f61814b662
Trevor Smith <tresmith@squeeze.local>
2026-01-05 12:44:53 -0700

    back to orginal fix

--- pages/index.js
+++ pages/index.js
@@ -3,21 +3,22 @@ import { validationConfig, initialTodos } from "../utils/constants.js";
 import Todo from "../components/Todo.js";
 import FormValidator from "../components/FormValidator.js";
 
+// Elements
 const addTodoButton = document.querySelector(".button_action_add");
 const addTodoPopup = document.querySelector("#add-todo-popup");
 const addTodoForm = addTodoPopup.querySelector(".popup__form");
 const addTodoCloseBtn = addTodoPopup.querySelector(".popup__close");
 const todosList = document.querySelector(".todos__list");
 
-const nameInput = addTodoForm.querySelector('input[name="name"]');
-const dateInput = addTodoForm.querySelector('input[name="date"]');
-
+// Modal helpers
 const openModal = (modal) => modal.classList.add("popup_visible");
 const closeModal = (modal) => modal.classList.remove("popup_visible");
 
+// ✅ Create validator ONCE
 const addTodoValidator = new FormValidator(validationConfig, addTodoForm);
 addTodoValidator.enableValidation();
 
+// Create a todo element using the Todo class
 const generateTodo = (data) => {
   const todo = new Todo(data, "#todo-template");
   return todo.getView();
@@ -28,26 +29,36 @@ function renderTodo(todoData) {
   todosList.append(todoEl);
 }
 
-// ✅ Render initial todos ONCE
-initialTodos.forEach(renderTodo);
+initialTodos.forEach((todo) => renderTodo(todo));
 
-addTodoButton.addEventListener("click", () => openModal(addTodoPopup));
-addTodoCloseBtn.addEventListener("click", () => closeModal(addTodoPopup));
+// Open/close popup
+addTodoButton.addEventListener("click", () => {
+  openModal(addTodoPopup);
+});
 
+addTodoCloseBtn.addEventListener("click", () => {
+  closeModal(addTodoPopup);
+});
+
+// Submit handler
 addTodoForm.addEventListener("submit", (evt) => {
   evt.preventDefault();
+
+  // Guard: only proceed if valid
   if (!addTodoForm.checkValidity()) return;
 
   const todoData = {
     id: uuidv4(),
-    name: nameInput.value,
-    date: dateInput.value,
+    name: addTodoForm.elements.name.value,
+    date: addTodoForm.elements.date.value,
     completed: false,
   };
 
   renderTodo(todoData);
 
+  // ✅ Task 6: reset form + validation only after successful submit
   addTodoForm.reset();
   addTodoValidator.resetValidation();
+
   closeModal(addTodoPopup);
 });
